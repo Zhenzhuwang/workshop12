258145e786b8b901fb385506d7e02ace79e67bbe
zhenzhu <wangzhenzhu37@gmail.com>
2023-01-04 16:24:30 +0800

    generate random number with image

---
+++ src/main/java/sg/edu/us/iss/app/workshop12/exception/RandNoException.java
@@ -0,0 +1,7 @@
+package sg.edu.us.iss.app.workshop12.exception;
+
+public class RandNoException extends RuntimeException {
+
+    private static final long serialVersionYID = 1L;
+    
+}
---
+++ src/main/java/sg/edu/us/iss/app/workshop12/models/Generate.java
@@ -0,0 +1,15 @@
+package sg.edu.us.iss.app.workshop12.models;
+
+public class Generate {
+
+    private int numberVal;
+
+    public int getNumberVal() {
+        return numberVal;
+    }
+
+    public void setNumberVal(int numberVal) {
+        this.numberVal = numberVal;
+    }
+    
+}
